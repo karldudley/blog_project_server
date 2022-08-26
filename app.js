540d0f9bd7307b95a5c562a3202b297615ec1121
Camila Pessoa <caape13@hotmail.com>
2022-08-26 13:07:52 +0100

    add get / and get /post functions

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+const data = require('./posts.json')
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
+app.get('/posts', (req, res) => {
+    res.json(data)
+})
+
+module.exports = app;
