b7958c5585e2091210c8c2d1412304a0eaa4595a
Camila Pessoa <caape13@hotmail.com>
2022-08-26 14:59:20 +0100

    add /post function to app.js

--- app.js
+++ app.js
@@ -15,4 +15,27 @@ app.get('/posts', (req, res) => {
     res.json(data)
 })
 
+app.get('/posts/:id', (req, res) => {
+    
+    if (data.posts[req.params.id-1]) {
+        res.send(data.posts[req.params.id-1])
+       
+    } else {
+            res.status(404).send({error: `Choose a number between 1 and ${data.length}`})
+        }
+    })
+
+app.post('/posts', (req, res) => {
+
+    const newMessage = req.body.message
+    const newPostId = (parseInt(data.posts[data.posts.length - 1].id)+1).toString()
+
+    let newPost = {id: newPostId, message: newMessage}
+
+    data.posts.push(newPost)
+
+    res.status(201).send(newPost)
+
+})
+
 module.exports = app;
