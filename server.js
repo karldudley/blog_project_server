54a71852d427b01b68ca6de983df1dcaa1ed95b7
Camila Pessoa <caape13@hotmail.com>
2022-08-26 15:35:32 +0100

    add Procfile

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+    console.log(`Express departing from port ${port}!`)
 })
