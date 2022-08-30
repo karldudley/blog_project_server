fd4b0e0f16443aae4974a807014db55c38779439
Camila Pessoa <caape13@hotmail.com>
2022-08-30 13:08:46 +0100

    implement get comments function

--- app.js
+++ app.js
@@ -29,15 +29,54 @@ app.get('/posts/:id', (req, res) => {
 app.post('/posts', (req, res) => {
 
     const newPostId = (parseInt(data.posts[data.posts.length - 1].id)+1).toString();
-    const newMessage = req.body.message;
-    
+    const newTitle = req.body.title;
+    const newDesc = req.body.description;
+    const newContent = req.body.content;
+    const newGif = req.body.gif;
 
-    let newPost = {id: newPostId, message: newMessage}
+
+    let newPost = {id: newPostId, title: newTitle, description: newDesc, content: newContent, gif: newGif}
 
     data.posts.push(newPost)
 
     res.status(201).send(newPost)
 
 })
+
+app.get('/posts/:id/comments', (req, res) => {
+
+
+  if (data.posts[req.params.id-1].comments){
+     
+    res.json(data.posts[req.params.id-1].comments)
+    
+  } else {
+
+    res.status(404).send({error: `Choose a number between 1 and ${data.length}`})
+    
+  }
+
+})
+
+// app.get('/posts/:id/comments/:id', (req, res) => {
+
+//   if (data.posts[req.params.id-1]) {
+
+//     if (data.posts[req.params.id-1].comments){
+      
+//       res.json(data.posts[req.params.id-1].comments)
+    
+//     } else {
+      
+//       res.status(404).send({error: `Choose a number between 1 and ${data.length}`})
+//   }
+
+// } else {
+//         res.status(404).send({error: `Choose a number between 1 and ${data.length}`})
+//     }
+// })
+
+app.post('/posts/:id/comments')
+
 
 module.exports = app;
